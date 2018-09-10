3eebeb71e05c88b9333e457dcded966def922abc
Abstraction1 <humancollection@gmail.com>
2018-09-10 17:51:22 +0300

    add acceleration

--- bullet.h
+++ bullet.h
@@ -11,12 +11,13 @@
 class bullet
 {
 public:
-    bullet(sf::Texture *t, sf::Vector2f position
-           , sf::Vector2f max_velocity = sf::Vector2f(15.f, 0.f));
+    bullet(sf::Texture *t, sf::Vector2f position,  
+        sf::Vector2f direction, float initial_velocity,
+        float max_velocity, float acceleration);
     virtual ~bullet();
 
     //accessors
-    inline const sf::FloatRect& get_global_bounds() const { return this->sprite_.getGlobalBounds();}
+    inline const sf::FloatRect get_global_bounds() const { return this->sprite_.getGlobalBounds();}
     inline const sf::Vector2f& get_position() const { return this->sprite_.getPosition(); }
 
     //functions
@@ -25,9 +26,13 @@ public:
     void draw(sf::RenderTarget &target);
 
 private:
-    sf::Vector2f max_velocity_;
+    sf::Vector2f current_velocity_;
+    float max_velocity_;
+    float acceleration_;
+    sf::Vector2f direction_;
     sf::Texture *texture_;
     sf::Sprite sprite_;
+    sf::Vector2f player_center_;
 };
 
 #endif // BULLET_H
