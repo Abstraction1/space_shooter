d01eb554fcbfdc9d0a9c47d9c79e943cd4c994e8
Abstraction1 <humancollection@gmail.com>
2018-09-11 03:00:08 +0300

    add acceleration

--- player.cpp
+++ player.cpp
@@ -4,20 +4,16 @@ unsigned player::players = 0;
 
 enum controls { UP = 0, DOWN, LEFT, RIGHT, SHOOT };
 
-player::player(sf::Texture *t, sf::Texture *bullet_t,
-       int UP, int DOWN,
-       int LEFT, int RIGHT,
-       int SHOOT)
-
-    : level_(1), exp_(0), exp_next_(100)
-    , hp_(10), hp_next_(10), damage_(1)
-    , damage_max_(2)
-    , score_(0)
+player::player(sf::Texture *t, sf::Texture *bullet_t, sf::Texture *main_gun_texture, int UP, int DOWN, int LEFT, int RIGHT, int SHOOT)
+    : level_(1), exp_(0), exp_next_(100), hp_(10), hp_next_(10), damage_(1), damage_max_(2), score_(0)
 {
     this->texture_ = t;
     this->bullet_texture_ = bullet_t;
     this->sprite_.setTexture(*this->texture_);
     this->sprite_.setScale(0.13f, 0.13f);
+
+    this->main_gun_texture_ = main_gun_texture;
+    this->main_gun_sprite_.setTexture(*this->main_gun_texture_);
 
     this->shooter_timer_max_ = 25;
     this->shooter_timer_ = this->shooter_timer_max_;
@@ -29,11 +25,13 @@ player::player(sf::Texture *t, sf::Texture *bullet_t,
     this->controls[controls::LEFT] = LEFT;
     this->controls[controls::RIGHT] = RIGHT;
     this->controls[controls::SHOOT] = SHOOT;
+    
+    this->max_velocity_ = 25.f;
+    this->acceleration_ = 0.8f;
+    this->stabilizer_force_ = 0.4f; 
 
     this->player_number = player::players;
     player::players++;
-
-    std::cout << this->player_number << std::endl;
 }
 
 player::~player()
@@ -41,30 +39,79 @@ player::~player()
 
 }
 
+void player::update_accessories()
+{
+    this->main_gun_sprite_.setPosition(
+            this->player_center_.x - this->main_gun_sprite_.getGlobalBounds().width,
+            this->player_center_.y - this->main_gun_sprite_.getGlobalBounds().height); 
+}
+
 void player::movement()
 {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(this->controls[controls::UP]))) {
-        this->sprite_.move(0.f, -10.f);
+       this->direction_.x = 0.f; 
+       this->direction_.y = -1.f; 
+
+       if(this->current_velocity_.y > -this->max_velocity_ && direction_.y < 0)
+          this->current_velocity_.y += this->direction_.y * this->acceleration_;
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(this->controls[controls::DOWN]))) {
-        this->sprite_.move(0.f, 10.f);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(this->controls[controls::DOWN]))) { 
+       this->direction_.x = 0.f; 
+       this->direction_.y = 1.f; 
+
+       if(this->current_velocity_.y < this->max_velocity_ && direction_.y > 0)
+          this->current_velocity_.y += this->direction_.y * this->acceleration_;
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(this->controls[controls::LEFT]))) {
-        this->sprite_.move(-10.f, 0.f);
+       this->direction_.x = -1.f; 
+       this->direction_.y = 0.f; 
+
+       if(this->current_velocity_.x > -this->max_velocity_ && direction_.x < 0)
+          this->current_velocity_.x += this->direction_.x * this->acceleration_;
+
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(this->controls[controls::RIGHT]))) {
-        this->sprite_.move(10.f, 0.f);
+       this->direction_.x = 1.f; 
+       this->direction_.y = 0.f; 
+
+       if(this->current_velocity_.x < this->max_velocity_ && direction_.x > 0)
+          this->current_velocity_.x += this->direction_.x * this->acceleration_;
     }
+    
+    // DRAG FORE
+    
+    if(this->current_velocity_.x > 0) {
+        this->current_velocity_.x -= this->stabilizer_force_;
+        if(this->current_velocity_.x < 0) {
+            this->current_velocity_.x = 0;
+        }
+    }
+    else if(this->current_velocity_.x < 0) {
+        this->current_velocity_.x += this->stabilizer_force_;
+        if(this->current_velocity_.x > 0) {
+            this->current_velocity_.x = 0;
+        }
+    }
+    if(this->current_velocity_.y > 0) {
+        this->current_velocity_.y -= this->stabilizer_force_;
+        if(this->current_velocity_.y < 0) {
+            this->current_velocity_.y = 0;
+        }
+    }
+    else if(this->current_velocity_.y < 0) {
+        this->current_velocity_.y += this->stabilizer_force_;
+        if(this->current_velocity_.y > 0) {
+            this->current_velocity_.y = 0;
+        }
+    }
+
+    //final move
+    this->sprite_.move(this->current_velocity_.x, this->current_velocity_.y);
 }
 
 void player::combat() {
-    if (sf::Keyboard::isKeyPressed(
-        sf::Keyboard::Key(this->controls[controls::SHOOT]))
-            && this->shooter_timer_ >= shooter_timer_max_) {
-        this->bullets_.push_back(
-                bullet(bullet_texture_, this->player_center_, 
-                    sf::Vector2f(1.f,0.f), 5.f, 35.f, 0.5f));
-
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(this->controls[controls::SHOOT])) && this->shooter_timer_ >= shooter_timer_max_) {
+        this->bullets_.push_back(bullet(bullet_texture_, this->player_center_, sf::Vector2f(1.f,0.f), 2.f, 50.f, 1.f));
         this->shooter_timer_ = 0; // reset timer
     }
 }
@@ -81,11 +128,8 @@ void player::update(sf::Vector2u window_bounds)
     }
     
     //update position
-    
-    this->player_center_.x = 
-        this->sprite_.getPosition().x + this->sprite_.getGlobalBounds().width / 2;
-    this->player_center_.y = 
-        this->sprite_.getPosition().y + this->sprite_.getGlobalBounds().height / 2;
+    this->player_center_.x = this->sprite_.getPosition().x + this->sprite_.getGlobalBounds().width / 2;
+    this->player_center_.y = this->sprite_.getPosition().y + this->sprite_.getGlobalBounds().height / 2;
 
     this->movement();
     this->combat();
@@ -97,5 +141,7 @@ void player::draw(sf::RenderTarget &target)
         this->bullets_[i].draw(target);
     }
 
+    target.draw(this->main_gun_sprite_);
     target.draw(this->sprite_);
+
 }
